7792975dba62ab0bc8d142d6e9fa4501a93eeb8d
extezy158 <exxxtttezy@gmail.com>
2025-11-10 18:53:27 +0300

    js lesson
    +true, false, if ,else ,switch

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -38,5 +38,80 @@ let item={
 alert(item.tittle);
 let colors=['red', 'green', 'blue'];
 alert(colors[0]);
-let text='hello';
-alert()
+
+let temperature=25;
+if(temperature>=25){
+    console.log('на улице тепло')
+}
+let number=5;
+if(number<0){
+    console.log('отрицательное число')
+}
+
+let grade=9;
+if(grade>=9){
+    console.log('отлично')
+}
+else if(grade>=7){
+    console.log('хорошо')
+}
+else if(grade>=5){
+    console.log('удовлетворительно')
+}
+else{
+    console.log('нужно подтянуться')
+
+}
+
+let tmep=20;
+if(grade>=18){
+    console.log('тепло')
+}
+else if(grade>=13){
+    console.log('нормально')
+}
+else if(grade>=5){
+    console.log('прохладно')
+}
+else{
+    console.log('холодно')
+
+}
+
+let day=3;
+
+switch(day){
+    case 1:
+        console.log('понедельник')
+        break;
+    case 2:
+        console.log('вторник')
+        break;
+    case 3:
+        console.log('среда')
+        break;
+default:
+    console.log('другой день')
+}
+
+let role=1;
+switch(role){
+    case 1:
+        console.log('администратор')
+        break;
+    case 2:
+        console.log('юзер')
+        break;
+  case 3:
+        console.log('гость')
+        break;
+default:
+    console.log('роль не выбрана')
+}
+let isOnline=true;
+let status=isOnline?'online':'offline';
+console.log(status);
+
+let zakazreg=true;
+let zakazstatus=zakazreg?'заказ готов':'в сборке';
+console.log(zakazstatus);
