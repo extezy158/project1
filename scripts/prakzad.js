b29795c1a766973f393d0111fd592b4552f136a4
extezy158 <exxxtttezy@gmail.com>
2025-12-24 18:51:33 +0300

    lesson 24.12
    fdafdafafadfa

---
+++ scripts/prakzad.js
@@ -0,0 +1,67 @@
+console.log('1.');
+let a=10;
+let b=5;
+let c=a+b;
+console.log(c);
+
+console.log('2.');
+let name='JS';
+console.log('I love '+name);
+
+console.log('3.');
+let price=100;
+let discount=20;
+let itogcena=price-discount;
+console.log(itogcena);
+
+console.log('4.');
+let isOnline=true;
+let status=isOnline?'online':'offline';
+console.log(status);
+
+console.log('5.');
+let n=2;
+let nn=n*n;
+console.log(nn);
+
+console.log('6.');
+let message=null;
+
+console.log('7.');
+let m1=[1,2,3];
+console.log(m1.length);
+
+console.log('8.');
+let m2=['a','b','c'];
+console.log(m2[0]);
+
+console.log('9.');
+let m3=[1,2,3];
+m3.push(10);
+console.log(m3);
+
+console.log('10.');
+let m4=[1,2,3];
+m4.pop(3);
+
+console.log(m4);
+console.log('11.');
+let m5=[1,2,3];
+m5.forEach(element => console.log(element));
+
+console.log('12.');
+let m6=[1,2,3];
+let m6summ=m6.reduce((a,b)=>a+b);
+
+console.log('13.');
+
+console.log('14.');
+
+console.log('15.');
+let m9=[1,2,3];
+m9.forEach(element => console.log(element*2));
+
+console.log('16.');
+
+
+
